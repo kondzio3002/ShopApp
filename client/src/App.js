3b2f77a5e034ce97a10d896b6f587de878ba0c2d
Konrad Brożyna <kondzio3002@gmail.com>
2023-05-06 20:36:58 +0200

    Add base client

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,18 @@
 import { Routes, Route } from 'react-router-dom';
+import { Container } from 'react-bootstrap';
+import MainLayout from './components/layout/MainLayout/MainLayout';
+import Home from './components/pages/Home/Home';
 
 const App = () => {
-
+  return (
+    <Container>
+      <MainLayout>
+        <Routes>
+          <Route path='/' element={<Home />} />
+        </Routes>
+      </MainLayout>
+    </Container>
+  )
 }
 
 export default App;
--- client/src/redux/productsRedux.js
+++ client/src/redux/productsRedux.js
@@ -2,6 +2,10 @@ const reducerName = 'products';
 
 const createActionName = name => `app/${reducerName}/${name}`;
 
+const initialState = {
+  data: [],
+};
+
 export default function reducer(statePart = initialState, action = {}) {
   switch (action.type) {
     default:
