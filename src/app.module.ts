d1e90c1cb24afe6611d97399a9536b09ab0d4088
Konrad Brożyna <kondzio3002@gmail.com>
2023-05-06 12:59:34 +0200

    Add PrismaService and settings for main.ts and app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,18 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule, RequestMethod } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import * as cors from 'cors';
 
 @Module({
   imports: [],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer): void {
+    consumer.apply(cors()).forRoutes({
+      path: '*',
+      method: RequestMethod.ALL,
+    });
+  }
+}
