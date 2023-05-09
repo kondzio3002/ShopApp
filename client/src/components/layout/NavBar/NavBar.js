87203887243312a0b869f5ad380feadcd56039c1
Konrad Brożyna <kondzio3002@gmail.com>
2023-05-09 19:24:53 +0200

    Add icon for NavLink cart in NavBar

--- client/src/components/layout/NavBar/NavBar.js
+++ client/src/components/layout/NavBar/NavBar.js
@@ -1,4 +1,7 @@
-import { Container, Navbar, Nav } from 'react-bootstrap';
+import { Container, Navbar, Nav, Button } from 'react-bootstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBasketShopping } from '@fortawesome/free-solid-svg-icons';
+import styles from './NavBar.module.scss';
 
 const NavBar = () => {
   return (
@@ -7,7 +10,9 @@ const NavBar = () => {
         <Navbar.Brand>Shop App</Navbar.Brand>
         <Nav>
           <Nav.Link href='/'>Home</Nav.Link>
-          <Nav.Link href='/cart'>Cart</Nav.Link>
+        </Nav>
+        <Nav>
+          <Nav.Link href='/cart'><Button variant="warning"><FontAwesomeIcon className={styles.icon} icon={faBasketShopping}></FontAwesomeIcon></Button></Nav.Link>
         </Nav>
       </Container>
     </Navbar>
