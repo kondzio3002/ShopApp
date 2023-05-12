d678d32cd0e82c9b8d38936598d42668726d8cc0
Konrad Brożyna <kondzio3002@gmail.com>
2023-05-12 19:27:11 +0200

    Add endpoints getAll and getById for Orders

--- src/orders/orders.controller.ts
+++ src/orders/orders.controller.ts
@@ -1,7 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, NotFoundException, Param, ParseUUIDPipe } from '@nestjs/common';
 import { OrdersService } from './orders.service';
 
 @Controller('orders')
 export class OrdersController {
   constructor(private ordersService: OrdersService) {}
+
+  @Get('/')
+  getAll(): any {
+    return this.ordersService.getAll();
+  }
+
+  @Get('/:id')
+  async getById(@Param('id', new ParseUUIDPipe()) id: string) {
+    const order = await this.ordersService.getById(id);
+    if (!order) throw new NotFoundException("Order not found");
+    return order;
+  }
 }
--- src/orders/orders.service.ts
+++ src/orders/orders.service.ts
@@ -1,8 +1,21 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
-import { Order } from '@prisma/client';
+import { Order, ProductOnOrders } from '@prisma/client';
 
 @Injectable()
 export class OrdersService {
   constructor(private prismaService: PrismaService) {}
+
+  public getAll(): Promise<Order[]> {
+    return this.prismaService.order.findMany({
+      include: { products: true },
+    });
+  }
+
+  public getById(id: Order['id']): Promise<Order | null> {
+    return this.prismaService.order.findUnique({
+      where: { id },
+      include: { products: true }
+    });
+  }
 }
