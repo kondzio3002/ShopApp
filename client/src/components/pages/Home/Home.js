eeb279ceeae5239585b17aeb6904c04a573c4486
kondzio3002 <110060057+kondzio3002@users.noreply.github.com>
2023-05-30 21:29:29 +0200

    Add Gallery

--- client/src/components/pages/Home/Home.js
+++ client/src/components/pages/Home/Home.js
@@ -1,9 +1,11 @@
 import Products from "../../features/Products/Products";
+import Gallery from "../../views/Gallery/Gallery";
 
 const Home = () => {
   return (
     <div>
       <Products />
+      <Gallery />
     </div>
   );
 };
